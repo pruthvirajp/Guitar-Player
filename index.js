65ac8e49cb8c7978f5b0739366b80a4e78acf901
Pruthviraj Patil <83415580+pruthvirajp@users.noreply.github.com>
2021-07-14 13:15:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+document.getElementsByClassName("string1")[0].addEventListener('click', function () {
+  var audio = new Audio("string1.mp3");
+  audio.play();
+});
+
+document.getElementsByClassName("string2")[0].addEventListener('click', function () {
+  var audio = new Audio("string2.mp3");
+  audio.play();
+});
+
+
+document.getElementsByClassName("string3")[0].addEventListener('click', function () {
+  var audio = new Audio("string3.mp3");
+  audio.play();
+});
+
+
+document.getElementsByClassName("string4")[0].addEventListener('click', function () {
+  var audio = new Audio("string4.mp3");
+  audio.play();
+});
+
+
+document.getElementsByClassName("string5")[0].addEventListener('click', function () {
+  var audio = new Audio("string5.mp3");
+  audio.play();
+});
+
+
+document.getElementsByClassName("string6")[0].addEventListener('click', function () {
+  var audio = new Audio("string6.mp3");
+  audio.play();
+});
